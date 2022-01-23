72ceb9dd707ae09ec7bc7e707c364f380e4f3633
George Ellis <gellis7@gmail.com>
2022-01-23 17:22:44 +0800

    added the weather symbols and search history is stored in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var now = moment();
 var date;
 var currentTemperature = document.getElementById("currentTemperature");
+var weatherIcon = document.getElementById("weatherIcon");
 var currentWind = document.getElementById("currentWind");
 var currentHumidity = document.getElementById("currentHumidity");
 var UVIndex = document.getElementById("UVIndex");
@@ -11,14 +12,26 @@ var longitude;
 var forecast;
 var body = document.getElementsByTagName("body")[0];
 var searchCity = document.getElementById("searchCity");
+var PastSearchesDiv = document.getElementById("pastSearches");
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+if (searchHistory == undefined) {
+  searchHistory = [];
+} else {
+  for (let i = searchHistory.length - 1; i > -1; i--) {
+    city = searchHistory[i];
+    addToPastSearches();
+  }
+}
+
 var cityName = document.getElementById("cityName");
 var city = JSON.parse(localStorage.getItem("city"));
+
 if (city == undefined) {
   city = "Perth";
 }
 searchCity.value = city;
 cityName.innerText = city;
-var PastSearchesDiv = document.getElementById("pastSearches");
+
 var AddSearchHistory = false;
 getCurrentWeather(city);
 function getCurrentWeather(city) {
@@ -38,6 +51,7 @@ function getCurrentWeather(city) {
     })
     .then(function (data) {
       cityName.innerText = city + " " + moment().format("MM/DD/YYYY");
+
       console.log(data);
       currentTemperature.innerText = "Temp: " + data.main.temp + "9\xB0" + "F";
       currentWind.innerText = "Wind: " + data.wind.speed + " MPH";
@@ -47,11 +61,14 @@ function getCurrentWeather(city) {
       getUVIndex(latitude, longitude);
       if (AddSearchHistory) {
         addToPastSearches();
+        searchHistory.unshift(city);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
       }
       localStorage.setItem("city", JSON.stringify(city));
     })
     .catch(function (error) {
       searchCity.value = "Couldn't find " + searchCity.value + ", try again";
+      console.log(error);
     });
 }
 function getUVIndex(lat, lon) {
@@ -78,6 +95,12 @@ function getUVIndex(lat, lon) {
         UVcolor = "green";
       }
       UVIndex.setAttribute("style", "background-color:" + UVcolor + ";");
+      weatherIcon.setAttribute(
+        "src",
+        "http://openweathermap.org/img/w/" +
+          data.current.weather[0].icon +
+          ".png"
+      );
       forecast = data;
       fillforecasts();
     });
@@ -88,6 +111,13 @@ function fillforecasts() {
   for (let i = 0; i < 5; i++) {
     date = moment().add(i + 1, "days");
     forecastdiv.children[i].children[0].innerText = date.format("MM/DD/YYYY");
+    forecastdiv.children[i].children[1].setAttribute(
+      "src",
+      "http://openweathermap.org/img/w/" +
+        forecast.daily[i].weather[0].icon +
+        ".png"
+    );
+
     forecastdiv.children[i].children[2].innerText =
       "Temp: " + forecast.daily[i].temp.day + "9\xB0" + "F";
     forecastdiv.children[i].children[3].innerText =
@@ -135,12 +165,18 @@ function addToPastSearches() {
   var SearchHistoryButton = document.createElement("button");
   SearchHistoryButton.innerText = city;
   SearchHistoryButton.setAttribute("class", "btn btn-secondary col-md-12");
-  if (PastSearchesDiv.hasChildNodes) {
-    PastSearchesDiv.insertBefore(
-      SearchHistoryButton,
-      PastSearchesDiv.children[0]
-    );
+  if (
+    PastSearchesDiv.hasChildNodes &&
+    PastSearchesDiv.children[0] !== undefined
+  ) {
+    if (city !== PastSearchesDiv.children[0].innerText) {
+      PastSearchesDiv.insertBefore(
+        SearchHistoryButton,
+        PastSearchesDiv.children[0]
+      );
+    }
   } else {
     PastSearchesDiv.appendChild(SearchHistoryButton);
   }
+  localStorage.setItem("PastSearchesDiv", JSON.stringify(PastSearchesDiv));
 }
